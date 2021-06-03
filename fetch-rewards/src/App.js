2c526c5a9e95d1989a17c6cf9de7f2f05462b501
Kristiana Blake <kristiana.blake@gmail.com>
2021-06-03 15:30:27 -0500

    removed extra fluff from app.js

--- fetch-rewards/src/App.js
+++ fetch-rewards/src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
 import DataTable from './Components/Table.js';
-import Records from './Components/Records.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <DataTable/>
-      <Records/> 
     </div>
   );
 }
