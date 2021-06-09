ce3dd848da3545dbe65b88431da306f990aea4be
Kristiana Blake <kristiana.blake@gmail.com>
2021-06-08 20:47:13 -0500

    useEffect and axios are working

--- fetch-rewards/src/Components/RecordsList.js
+++ fetch-rewards/src/Components/RecordsList.js
@@ -1,53 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import { Table } from 'semantic-ui-react';
 
-export default class RecordsList extends React.Component {
-  state = {
-      records: []
-  }
+export default function RecordsList() {
 
-  componentDidMount() {
-      axios.get('https://fetch-hiring.s3.amazonaws.com/hiring.json')
-        .then(res => this.setState({records: res.data}))
-        console.log(records)
-        .catch(error => {
-          this.setState({ errorMessage: error.message });
-          console.error('There was an error!', error)
-        });
-  }
-  
-  // display all items grouped by listId
+  let [data, setData] = useState([]);
 
-  //function for Names 
+  useEffect(() => {
+    axios
+      .get('https://fetch-hiring.s3.amazonaws.com/hiring.json')
+      .then(response => setData(response.data))
+      .catch(error => {
+        console.log('There was an error!', error)
+      });
+    }, []);
 
-  render(){
-
-    const records = this.state.records.filter(record => !record).map((record, i));
-
-    return (
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>
-              Id
-            </Table.HeaderCell>
-            <Table.HeaderCell
-            >
-              List Id
-            </Table.HeaderCell>
-            <Table.HeaderCell
-            >
-              Name
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row key = {record.id}>
-              <Table.Cell>{records}</Table.Cell>
-          </Table.Row>
-          </Table.Body>
-      </Table>
-    )
-  }
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>
+            Id
+          </Table.HeaderCell>
+          <Table.HeaderCell
+          >
+            List Id
+          </Table.HeaderCell>
+          <Table.HeaderCell
+          >
+            Name
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row>
+            <Table.Cell><code>{JSON.stringify(data)}</code></Table.Cell>
+        </Table.Row>
+        </Table.Body>
+    </Table>
+  )
 }
